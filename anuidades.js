f5ffa314ec88145a5104facc2fbaf4e8001d74c0
FelipePsik <109187687+FelipePsik@users.noreply.github.com>
2026-01-02 16:38:58 -0300

    Atualização Anual das Anuidades e Taxas

--- anuidades.js
+++ anuidades.js
@@ -1,8 +1,8 @@
 export const valores = {
-  anuidade_aux: 224.27,
-  anuidade_tec: 274.29,
-  anuidade_enf: 379.55,
-  taxaInscricao: 260.09,
-  taxaReab: 271.92,
-  taxaSec: 260.09,
+  anuidade_aux: 235.60,
+  anuidade_tec: 288.14,
+  anuidade_enf: 398.72,
+  taxaInscricao: 273.23,
+  taxaReab: 285.66,
+  taxaSec: 273.23,
 };
